da18e4411f636e1a05340d2de094330a656a9775
Jaden Holladay <holladaystudio@gmail.com>
2018-09-09 03:11:57 -0600

    gql playground

--- app/server.js
+++ app/server.js
@@ -77,6 +77,71 @@ import { Facebook, FacebookApiException } from 'fb';
 const fb = new Facebook();
 // ============== /API =============
 
+// ============== GRAPHQL =============
+import { ApolloServer, graphiqlExpress } from 'apollo-server-express';
+import { express as voyagerMiddleware } from 'graphql-voyager/middleware';
+
+// Sets the req.user if there is a provided jwt token.
+function Authenticate(req, res, next)
+{
+  passport.authenticate('jwt', (err, user, info) => {
+   if (err) return next(err);
+   if (info)
+   {
+     if (info == 'TokenExpiredError: jwt expired')
+     {
+       // Clear out the users' jwt token.
+       res.clearCookie('jwt');
+     }
+     else if (info != 'Error: No auth token')
+     {
+       return next(info);
+     }
+   }
+   req.user = user || null;
+   next();
+ })(req, res, next);
+}
+
+const formatError = error => {
+  const message = error.originalError ? error.originalError.message : 'An unkown error occured';
+  const details = error.message || 'An unknown error occurred.';
+  const locations = error.locations;
+  const path = error.path;
+  const extensions = error.extensions;
+  const stack = DEVELOPMENT ? error.originalError : `ONLY AVAILABLE IN DEVELOPMENT. CHECK SERVER LOGS. DATE: ${new Date()}`;
+
+  console.error(error.originalError);
+
+  return extensions
+    ? { message, details, stack, locations, path, extensions }
+    : { message, details, stack, locations, path };
+}
+
+const context = ({req, res}) => {
+  return require('./context')({req, db, res, fb})
+}
+
+import { default as graphqlSchema } from './schema';
+
+const server = new ApolloServer({
+  schema: graphqlSchema,
+  context: context,
+  tracing: true,
+  cacheControl: true,
+  introspection: true,
+  playground: true,
+  formatError
+  //extensions: [ProfilerExtension]
+})
+server.applyMiddleware({ app });
+
+app.use('/graphql', Authenticate);
+// app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
+app.use('/voyager', voyagerMiddleware({ endpointUrl: '/graphql' }));
+
+// ============== /GRAPHQL =============
+
 // ============== REACT SERVER SIDE RENDERING =============
 
 //
@@ -150,69 +215,6 @@ app.get('*', async (req, res, next) => {
 });
 // ============== /REACT SERVER SIDE RENDERING =============
 
-// ============== GRAPHQL =============
-import { ApolloServer, graphiqlExpress } from 'apollo-server-express';
-import { express as voyagerMiddleware } from 'graphql-voyager/middleware';
-
-// Sets the req.user if there is a provided jwt token.
-function Authenticate(req, res, next)
-{
-  passport.authenticate('jwt', (err, user, info) => {
-   if (err) return next(err);
-   if (info)
-   {
-     if (info == 'TokenExpiredError: jwt expired')
-     {
-       // Clear out the users' jwt token.
-       res.clearCookie('jwt');
-     }
-     else if (info != 'Error: No auth token')
-     {
-       return next(info);
-     }
-   }
-   req.user = user || null;
-   next();
- })(req, res, next);
-}
-
-const formatError = error => {
-  const message = error.originalError ? error.originalError.message : 'An unkown error occured';
-  const details = error.message || 'An unknown error occurred.';
-  const locations = error.locations;
-  const path = error.path;
-  const extensions = error.extensions;
-  const stack = DEVELOPMENT ? error.originalError : `ONLY AVAILABLE IN DEVELOPMENT. CHECK SERVER LOGS. DATE: ${new Date()}`;
-
-  console.error(error.originalError);
-
-  return extensions
-    ? { message, details, stack, locations, path, extensions }
-    : { message, details, stack, locations, path };
-}
-
-const context = ({req, res}) => {
-  return require('./context')({req, db, res, fb})
-}
-
-import { default as graphqlSchema } from './schema';
-
-const server = new ApolloServer({
-  schema: graphqlSchema,
-  context: context,
-  tracing: true,
-  cacheControl: true,
-  introspection: true,
-  formatError
-  //extensions: [ProfilerExtension]
-})
-server.applyMiddleware({ app });
-
-app.use('/graphql', Authenticate);
-// app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
-app.use('/voyager', voyagerMiddleware({ endpointUrl: '/graphql' }));
-
-// ============== /GRAPHQL =============
 app.listen({ port: process.env.PORT }, () =>
   console.log(`🚀 Server ready at ${process.env.WEB_URI}${server.graphqlPath}`)
 );
