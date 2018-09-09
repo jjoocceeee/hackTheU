f30488eb03fc87fc5060f914db5989032b6b712b
JoCee <joceeporter@outlook.com>
2018-09-09 10:02:33 -0600

    a few more resolvers

--- app/models/user.js
+++ app/models/user.js
@@ -15,7 +15,7 @@ const UserSchema = new mongoose.Schema({
     index: true
   },
   maxSavings: {
-    type: String,
+    type: Number,
     required: true,
     description: "Max amount of money to put into savings account",
     index: true
@@ -44,20 +44,17 @@ UserSchema.pre('save', async function(next)
 {
   var user = this;
   // Copy the username field into the unique.
-  user.username_unique = user.username;
+  // user.username_unique = user.username;
   if (this.isNew)
   {
     const bankAccount = await BankAccount.create({
       checking: 500,
       savings: 0
     });
-    this.bankingid = banking._id;
+    // this.bankingid = banking._id;
   }
   return next();
 });
-
-
-
 
 
 export const User = mongoose.model('User', UserSchema);
